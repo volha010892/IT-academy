1a0c105b42aff97db2b02e7f2f53947c125ae38c
volha010892 <66121147+volha010892@users.noreply.github.com>
2020-07-17 21:59:34 +0300

    Update MyShop.js

--- ishop3/components/MyShop.js
+++ ishop3/components/MyShop.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DOM from 'react-dom-factories';
 class MyShop extends React.Component{
+  static propTypes = {
+    code:PropTypes.number.isRequired,
+    titleName:PropTypes.string.isRequired,
+  }
     render(){
         var titl=this.props.title.map( v =>
             <th key={v.code}>{v.titleName}</th>
