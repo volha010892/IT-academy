91f5338ab2ce6e7dc2768099fe87394fd4713c1e
volha010892 <66121147+volha010892@users.noreply.github.com>
2020-07-24 12:28:46 +0300

    commit

--- br2jsx/Frame.js
+++ br2jsx/Frame.js
@@ -4,17 +4,15 @@ import PropTypes from 'prop-types';
 import './Frame.css';
 
 class Frame extends React.Component {
-    
       render() {
           var items=[];
           var item=[];
-         items=this.props.text.split(/(<\s*br\s*\/*>)/);
+         items=this.props.text.split(/<\s*br\s*\/*>/);
           items.map((v, i)=>{
-              if(v.match(RegExp("<\\s*br\\s*\/*>"))){          
-              item.push(<br key={i}></br>);
-              }
-              else item.push(v);
-          })
+              item.push(v)
+              item.push(<br key={i}></br>)
+          }
+         )
         return (
         <div className='br2jsx'>{item}</div>
         )
