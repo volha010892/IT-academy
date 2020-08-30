c1188ed263ecbced56384f418f6dd99212d08611
volha010892 <66121147+volha010892@users.noreply.github.com>
2020-08-30 18:16:47 +0300

    commit

--- Scales/App.ts
+++ Scales/App.ts
@@ -1,4 +1,66 @@
+interface IScalable{
+    getScale():number;
+    getName():string;
+}
 class Scales{
+products:IScalable[]=[];
+addProduct(p:IScalable):void{
+    this.products.push(p);
+}
+getSumScale():number{
+    let sumScale:number=0;
+    for ( let testnum:number=0; testnum<this.products.length; testnum++ ) {
+        sumScale+=this.products[testnum].getScale();
+   }  
+   return sumScale;
+}
+getNameList():Array<String>{
+    let nameList:Array<String>=[];
+    for ( let testnum:number=0; testnum<this.products.length; testnum++ ) {
+     let name=this.products[testnum].getName();
+     nameList.push(name);
+    }
+    return nameList;
+}
+}
+
+class Apple implements IScalable{
+
+    weight:number;
+    name:string;
+
+    constructor(_name:string, _weight:number) {
+        this.name=_name;
+        this.weight=_weight; 
+    }
+        getScale():number {
+            return this.weight;
+        }
+    
+        getName():string {
+            return this.name;
+        }
+    
+}
+class Tomato implements IScalable{
+
+    weight:number;
+    name:string;
+
+    constructor(_name:string, _weight:number) {
+        this.name=_name;
+        this.weight=_weight; 
+    }
+        getScale():number {
+            return this.weight;
+        }
+    
+        getName():string {
+            return this.name;
+        }
+    
+}
+/*class Scales{
     items:Array<Product>;
 
     constructor() {
@@ -63,14 +125,14 @@ class Tomato extends Product {
     }
 
 }
-
+*/
 let scale:Scales=new Scales();
 let apple1:Apple=new Apple('яблоко красное', 45);
 let apple2:Apple=new Apple('яблоко зеленое', 125);
 let tomato1:Tomato=new Tomato('томат малиновый', 56);
-scale.add(apple1);
-scale.add(apple2);
-scale.add(tomato1);
+scale.addProduct(apple1);
+scale.addProduct(apple2);
+scale.addProduct(tomato1);
 let nameList:Array<String>=scale.getNameList();
 console.log('Список продуктов: '+ nameList)
 let sum:number=scale.getSumScale();
